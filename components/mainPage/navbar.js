ceb8d3f813286a4cc6a307146b5040909586726e
Murat Şahin <murat.sahin.dev@gmail.com>
2023-07-15 03:01:10 +0300

    feature: Fully responsive navbar is added.

--- components/mainPage/navbar.js
+++ components/mainPage/navbar.js
@@ -1,26 +1,29 @@
 import styles from "./navbar.module.css";
 import Image from "next/image";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
   return (
-    <div className={styles.navbarContainer}>
-      <div className={styles.navbarContent}>
-        <div className={styles.box}>
+    <div
+      className={`${styles.navbarContainer} ${styles.mobileNavbarContainer}`}
+    >
+      <div className={`${styles.navbarContent} ${styles.mobileContent}`}>
+        <div className={`${styles.logoBox} ${styles.mobileLogoBox}`}>
           <Image
-          className={styles.logoImage}
+            className={`${styles.logoImage} ${styles.mobileLogoImage}`}
             src="/images/logo.gif"
             height={0}
             width={50}
             alt="Hourglass Logo"
           />
-          <div style={{width:15}}>
-
-          </div>
+          <div style={{ width: 15 }}></div>
           <div className={styles.logoTextWrapper}>
-            <p className={styles.logoText}>muratsahin.dev</p>
+            <p className={`${styles.logoText} ${styles.mobileLogoText}`}>
+              muratsahin.dev
+            </p>
           </div>
         </div>
-        <div className={styles.middleBox}>
+        <div className={`${styles.hiddenMiddleComponent} ${styles.middleBox}`}>
           <div className={styles.middleTextWrapper}>
             <p className={styles.middleTextButton}>portfolio</p>
           </div>
@@ -31,7 +34,16 @@ export default function Navbar() {
             <p className={styles.middleTextButton}>contact</p>
           </div>
         </div>
-        <div className={styles.box}></div>
+        <div className={`${styles.hiddenComponent} ${styles.box}`}></div>
+        <div
+          className={`${styles.slideScreenButtonBox} ${styles.appearSlideButton} ${styles.appearSlideButton}`}
+        >
+          <div className={`${styles.menuIconWrapper}`}>
+            <AiOutlineMenu
+              className={`${styles.menuIcon} ${styles.mobileMenuIcon}`}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
