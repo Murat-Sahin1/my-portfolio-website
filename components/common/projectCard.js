e93de14608637d6c748f9c841d8f871d4cab4e45
Murat Şahin <murat.sahin.dev@gmail.com>
2023-08-23 10:13:44 +0300

    interactive portfolio cards.

---
+++ components/common/projectCard.js
@@ -0,0 +1,42 @@
+import styles from "./projectCard.module.css";
+import Image from "next/image";
+
+export default function ProjectCard(props) {
+  return (
+    <>
+      <div class={styles.cardContainer}>
+        <div class={styles.imageContainer}>
+          <img
+            class={styles.image}
+            src="/images/desert.jpg"
+            height={600}
+            width={600}
+            draggable={false}
+            style={{ objectPosition: `${((props.precentage-(-126)) / (0-(-126))) * 100}% 100%` }}
+            
+          />
+        </div>
+        <div class={styles.textContainer}>
+          <div class={styles.projectTagContainer}>
+            <p class={styles.projectTagText}>WEBSITE</p>
+          </div>
+          <div class={styles.projectHeaderContainer}>
+            <p class={styles.projectHeaderText}>Off-Tone</p>
+          </div>
+          <div class={styles.projectDescContainer}>
+            <p class={styles.projectDescription}>
+              Through art management and webdesign principles I help my clients
+              around the world grow their business. Through art management and
+              webdesign principles I help my clients around the world grow their
+              business. Through art management and webdesign principles I help
+              my clients around the world grow their business.{" "}
+            </p>
+          </div>
+          <div class={styles.dateContainer}>
+            <p class={styles.dateText}>13th Dec, 2016 </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- components/mainPage/portfolioFrame/portfolioFrameContainer.js
+++ components/mainPage/portfolioFrame/portfolioFrameContainer.js
@@ -1,9 +1,66 @@
 import styles from "./portfolioFrameContainer.module.css";
+import { useState, useEffect } from "react";
+import ProjectCard from "@/components/common/projectCard";
 
 export default function PortfolioFrameContainer() {
+  const [isDragging, setIsDragging] = useState(false);
+  const [clickedX, setClickedX] = useState(null);
+  const [precentage, setPrecentage] = useState(0);
+  const [prevPrecentage, setPrevPrecentage] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  const handleClick = (event) => {
+    setIsDragging(true);
+    const xPosition = event.clientX;
+    setClickedX(xPosition);
+    console.log(isDragging);
+  };
+
+  const handleMouseUp = () => {
+    if (isDragging) {
+      setIsDragging(false);
+      console.log(isDragging);
+      setPrevPrecentage(precentage);
+    }
+  };
+
+  const handleMouseMove = (event) => {
+    if (isDragging) {
+      const maxValue = windowWidth / 2;
+      const totalDrag = event.clientX - clickedX;
+      var precent = ((totalDrag / maxValue) * 100) + prevPrecentage;
+      if (precent >= 0) {
+        precent = 0;
+      } else if (precent <= -126) {
+        precent = -126;
+      }
+      setPrecentage(precent);
+    }
+    console.log(precentage);
+  };
+
   return (
     <>
-      <div id={"portfolioFrame"} class={styles.portfolioFrameContainer}>
+      <div
+        id={"portfolioFrame"}
+        class={styles.portfolioFrameContainer}
+        onMouseDown={handleClick}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      >
         <div class={styles.headerContainer}>
           <div class={styles.portfolioTextContainer}>
             <p class={styles.portfolioText}>portfolio</p>
@@ -16,8 +73,22 @@ export default function PortfolioFrameContainer() {
           </div>
         </div>
         <div class={styles.projectsContainer}>
-          <div class={styles.projectStripe}>
-            
+          <div
+            class={styles.projectStripe}
+            style={{ transform: `translateX(${precentage}%)` }}
+          >
+            <div class={styles.project}>
+              <ProjectCard precentage={precentage}/>
+            </div>
+            <div class={styles.project}>
+              <ProjectCard precentage={precentage}/>
+            </div>
+            <div class={styles.project}>
+              <ProjectCard precentage={precentage}/>
+            </div>
+            <div class={styles.project}>
+              <ProjectCard precentage={precentage}/>
+            </div>
           </div>
         </div>
       </div>
