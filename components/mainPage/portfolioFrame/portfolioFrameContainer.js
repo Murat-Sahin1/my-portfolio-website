876be799cd16b282fb2f2ea3c8f41713536c746f
Murat Şahin <murat.sahin.dev@gmail.com>
2023-08-23 03:01:33 +0300

    portfolio page layout.

--- components/mainPage/portfolioFrame/portfolioFrameContainer.js
+++ components/mainPage/portfolioFrame/portfolioFrameContainer.js
@@ -1,9 +1,26 @@
 import styles from "./portfolioFrameContainer.module.css";
 
 export default function PortfolioFrameContainer() {
-    return(<>
-        <div id={"portfolioFrame"} class={styles.portfolioFrameContainer}>
-            <div class={styles.testingg}></div>
+  return (
+    <>
+      <div id={"portfolioFrame"} class={styles.portfolioFrameContainer}>
+        <div class={styles.headerContainer}>
+          <div class={styles.portfolioTextContainer}>
+            <p class={styles.portfolioText}>portfolio</p>
+          </div>
+          <div class={styles.descriptionTextContainer}>
+            <p class={styles.descriptionText}>
+              Through art management and webdesign principles I help my clients
+              around the world grow their business.
+            </p>
+          </div>
         </div>
-    </>)
+        <div class={styles.projectsContainer}>
+          <div class={styles.projectStripe}>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
